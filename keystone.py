e4996e36348f6901aab30edeea8f8bb501202b28
theodox <steve@theodox.com>
2018-11-13 01:30:40 -0800

    working; it would be nice if we could support more args from the command line though

---
+++ keystone.py
@@ -0,0 +1,128 @@
+"""
+Copyright 2018 Steve Theodore
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+import os
+import os.path
+import base64
+import zipfile
+import re
+import compileall
+import argparse
+
+
+PY_SHIM = '''
+import sys
+import os
+import os.path
+import runpy
+import maya.cmds
+
+location = os.path.dirname(sys.argv[1])
+lastmod = os.path.getmtime(location)
+zipfolder = maya.cmds.about(pd=True) + "/.keystone" 
+zipname = zipfolder + "/" + os.path.basename(sys.argv[1]) + ".zip"
+print "zipname", zipname
+if not (os.path.exists (zipname)) or os.path.getmtime(zipname) < lastmod:
+    if not os.path.exists(zipfolder):
+        os.makedirs(os.path.dirname(zipfolder))
+    with open(sys.argv[1], 'rb') as melfile:
+        melfile.seek(__keystone_offset)
+        with open(zipname, "wb") as output:
+            contents = melfile.read(40960)
+            while contents:
+                output.write(contents)
+                contents = melfile.read(40960)
+sys.path.insert(0, zipname)
+if __keystone_main:
+    runpy.run_path(zipname)
+
+'''
+
+MEL_SHIM = '''// keystone
+python("__keystone_offset = %s; __keystone_main = %i");
+python("import base64; __keystone_cmd = base64.urlsafe_b64decode('%s'); exec __keystone_cmd");
+'''
+
+
+def zip_directory(source, zipfilename, ignore=None, include=None):
+
+    compileall.compile_dir(source, maxlevels=24)
+
+    archive = zipfile.ZipFile(zipfilename, 'w')
+
+    py_re = re.compile(".py$")
+
+    if ignore:
+        ignore = re.compile(ignore)
+
+    if include:
+        include = re.compile(include)
+
+    def iter_files():
+        for root, dirs, files in os.walk(source):
+            for f in files:
+                fullpath = os.path.join(root, f).replace("\\", "/")
+                if any(py_re.findall(fullpath)):
+                    continue
+                if include is not None and any(include.findall(fullpath)):
+                    yield fullpath
+                    continue
+                if ignore is None or not ignore.findall(fullpath):
+                    yield fullpath
+
+    offset = len(source)
+    try:
+        for each_file in iter_files():
+            archive.write(each_file, each_file[offset:])
+    finally:
+        archive.close()
+
+    return zipfilename
+
+
+def generate_mel(melname, folder):
+
+    zipped = zip_directory(folder, 'keystone_temp.zip')
+    encoded = base64.urlsafe_b64encode(PY_SHIM)
+    has_main = os.path.exists(folder + "/__main__.py")
+    mel_text = MEL_SHIM % ("%s", has_main, encoded)
+    offset = len(mel_text) + 3  # get pass the
+    offset += len(str(offset))
+    mel_text = mel_text % str(offset)
+    with open(melname, 'wt') as output:
+        output.write(mel_text)
+        output.write('//')
+
+    with open(zipped, 'rb') as zipper:
+        with open(melname, 'ab') as mel:
+            mel.write(zipper.read())
+
+    os.remove(zipped)
+
+desc ='''
+Compiles a python project into an executable mel file.  If the project folder
+contains a __main__.py at the root level, it will be executed when the mel is launched. Any
+python modules in the project folder will be added to maya's python path.
+'''
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('melfile', action='store', help="path to the output mel file")
+    parser.add_argument('project', action='store', help="path to the project folder")
+
+    args = parser.parse_args()
+    
+    melfile = os.path.abspath(os.path.normpath(args.melfile))
+    if not melfile.endswith(".mel"):
+        melfile += ".mel"
+    project = os.path.abspath(os.path.normpath(args.project))
+
+    if not os.path.isdir(project):
+        raise ValueError("'%s' is not a directory" % project)
+
+    generate_mel(melfile, project)
